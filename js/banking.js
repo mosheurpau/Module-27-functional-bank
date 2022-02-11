c15f22318c8fd8cb2e6c620751b289eea48a4021
mosheurpau <mosheur.pau@gamil.com>
2022-02-11 19:28:52 +0600

    hanle withdrow button

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,4 @@
-// hanle withdraw button
+// hanle deposit button
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-input');
     const newDepositText = depositInput.value;
@@ -20,4 +20,28 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
 
     // clear input field
     depositInput.value = '';
+});
+
+// hanle withdrow button
+document.getElementById('withdrow-btn').addEventListener('click', function () {
+    const withdrowInput = document.getElementById('withdrow-input');
+    const newWithdrowText = withdrowInput.value;
+    const newWithdrowAmount = parseFloat(newWithdrowText);
+
+    // get current withdrow
+    const withdrowTotal = document.getElementById('withdrow-total');
+    const previousWithdrowText = withdrowTotal.innerText;
+    const previousWithdrowAmount = parseFloat(previousWithdrowText);
+
+    withdrowTotal.innerText = previousWithdrowAmount + newWithdrowAmount;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - newWithdrowAmount;
+
+    // clear input field
+    withdrowInput.value = '';
 });
